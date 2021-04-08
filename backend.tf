16635770ee7d40d3f718b2b6dcb2cf3d21283145
jiro1 <43940821+jiro1@users.noreply.github.com>
2021-04-07 19:55:15 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,6 +2,6 @@ terraform {
     backend "s3" {
         bucket = "jenkins-terraform-evolvecybertraining"
         key = "terraform_vpc/us-east-2/class/dev/infrastructure.tfstate"
-        region = "us-east-2"
+        region = "us-east-1"
     }
 }
